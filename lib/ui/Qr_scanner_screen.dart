3ad8a17be3749b8bd0852d2006feb7b9b45c2eb5
Avinash Kumar <bhaiavinash9@gmail.com>
2021-07-04 23:58:13 +0530

    Qr functionlity is working

--- lib/ui/Qr_scanner_screen.dart
+++ lib/ui/Qr_scanner_screen.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:oktoast/oktoast.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
-import 'package:qrscan/qrscan.dart' as scanner;
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:retroshare/common/color_loader_3.dart';
 import 'package:retroshare/common/styles.dart';
 import 'package:retroshare/provider/friendLocation.dart';
-
 import 'UpdateIdenityScreen.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
 
 class QRScanner extends StatefulWidget {
   final String qr_data;
@@ -23,8 +21,14 @@ class _QRScannerState extends State<QRScanner> {
   bool _requestCreateIdentity;
 
   Future _scan() async {
-    await Permission.camera.request();
-    String barcode = await scanner.scan();
+    String barcode = null;
+    try {
+      barcode = await scanner.scan();
+      print("hello");
+      print(barcode);
+    } catch (e) {
+      print(e);
+    }
     if (barcode == null) {
       setState(() {
         _requestCreateIdentity = false;
@@ -112,23 +116,26 @@ class _QRScannerState extends State<QRScanner> {
                         size: 320,
                         gapless: false,
                       ),
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(width: 2, color: Colors.black87),
-                          borderRadius: BorderRadius.all(Radius.circular(
-                                  50) //         <--- border radius here
-                              ),
-                        ),
-                        child: IconButton(
-                          icon: Icon(Icons.document_scanner),
-                          iconSize: 45.0,
-                          color: Colors.black87,
-                          onPressed: () async {
-                            setState(() {
-                              _requestCreateIdentity = true;
-                            });
-                            await _scan();
-                          },
+                      RaisedButton(
+                        elevation: 25,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 2, color: Colors.black87),
+                            borderRadius: BorderRadius.all(Radius.circular(
+                                    50) //         <--- border radius here
+                                ),
+                          ),
+                          child: IconButton(
+                            icon: Icon(Icons.document_scanner),
+                            iconSize: 45.0,
+                            color: Colors.black87,
+                            onPressed: () async {
+                              setState(() {
+                                _requestCreateIdentity = true;
+                              });
+                              await _scan();
+                            },
+                          ),
                         ),
                       )
                     ]))),
@@ -148,46 +155,63 @@ class _QRScannerState extends State<QRScanner> {
   }
 
   contentBox(context) {
-    return Stack(children: <Widget>[
-      Container(
-        padding: EdgeInsets.only(
-            left: Constants.padding,
-            top: Constants.avatarRadius,
-            right: Constants.padding,
-            bottom: Constants.padding),
-        margin: EdgeInsets.only(top: Constants.avatarRadius),
-        decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(Constants.padding),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black, offset: Offset(0, 10), blurRadius: 10),
-            ]),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text(
-              'something went Wrong!',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: FlatButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text(
-                    "OK",
-                    style: TextStyle(fontSize: 14),
-                  )),
-            ),
-          ],
+    return Stack(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(
+              left: Constants.padding,
+              top: Constants.avatarRadius,
+              right: Constants.padding,
+              bottom: Constants.padding),
+          margin: EdgeInsets.only(top: Constants.avatarRadius),
+          decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(Constants.padding),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black, offset: Offset(0, 10), blurRadius: 10),
+              ]),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                'something went Wrong!',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      "OK",
+                      style: TextStyle(fontSize: 14),
+                    )),
+              ),
+            ],
+          ),
         ),
-      )
-    ]);
+        Positioned(
+          left: Constants.padding,
+          right: Constants.padding,
+          child: CircleAvatar(
+            backgroundColor: Colors.transparent,
+            radius: Constants.avatarRadius,
+            child: ClipRRect(
+              borderRadius:
+                  BorderRadius.all(Radius.circular(Constants.avatarRadius)),
+              child: Image(
+                image: AssetImage('assets/rs-logo.png'),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
